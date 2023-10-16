812ede1db8960160da14944392ac7cfba1fc021b
Royserg <staniojakub@gmail.com>
2023-10-16 14:47:34 +0300

    chore: update Ulrs to get Weather data from Meteo

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,15 +1,12 @@
 class Urls {
-  static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
-  static const String apiKey = '609d67ae9abbd3612ae2f78e17da52e9';
-  static String currentWeatherByName(String cityName) =>
-      '$baseUrl/weather?q=$cityName&appid=$apiKey&units=metric';
-
-  static String weatherIcon(String iconCode) =>
-      'http://openweathermap.org/img/wn/$iconCode@2x.png';
-
   // -- Open Meteo API
-  static const String GeocodingOpenMeteoBaseUrl =
+  static const String geocodingOpenMeteoBaseUrl =
       'https://geocoding-api.open-meteo.com/v1/';
   static String searchCity(String query) =>
-      '$GeocodingOpenMeteoBaseUrl/search?language=en&count=5&format=json&name=$query';
+      '$geocodingOpenMeteoBaseUrl/search?language=en&count=5&format=json&name=$query';
+
+  static const String weatherOpenMeteoBaseUrl =
+      'https://api.open-meteo.com/v1/';
+  static String weatherByLatLong(double latitude, double longitude) =>
+      '$weatherOpenMeteoBaseUrl/forecast?latitude=$latitude&longitude=$longitude&current=temperature_2m,apparent_temperature,precipitation,weathercode&hourly=temperature_2m,weathercode,is_day&timezone=auto';
 }
